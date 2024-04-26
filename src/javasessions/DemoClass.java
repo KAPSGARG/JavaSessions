18b11375ed5d77e5be173520d2716b153f1b2413
KAPIL <kapilgarg660@gmail.com>
2024-04-26 12:13:29 +0530

    Added a new class

---
+++ src/javasessions/DemoClass.java
@@ -0,0 +1,12 @@
+package javasessions;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "Just for the testing purpose";
+
+	}
+
+}
