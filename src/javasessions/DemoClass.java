e58924cecc3c02163863fd3727cc9bd45f488446
KAPIL <kapilgarg660@gmail.com>
2024-04-26 12:23:51 +0530

    Removed the string variable

--- src/javasessions/DemoClass.java
+++ src/javasessions/DemoClass.java
@@ -5,8 +5,6 @@ public class DemoClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String name = "Just for the testing purpose";
-
 		int number = 10;
 
 	}
