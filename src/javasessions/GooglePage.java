9a00afde6ca237059dfdfcec6b68b0b7bc4739dd
KAPSGARG <167730427+KAPSGARG@users.noreply.github.com>
2024-05-02 11:46:34 +0530

    Create GooglePage.java

---
+++ src/javasessions/GooglePage.java
@@ -0,0 +1,11 @@
+package javasessions;
+
+public class GooglePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Google page class");
+
+	}
+
+}
